63a35dc8d372418df05afe7b80623ed6606a9157
Tajima Shingo <tajima-shingo@kayac.com>
2015-11-15 17:59:50 +0900

    ビール換算のみ実装

--- _build/src/coffee/pc/script.coffee
+++ _build/src/coffee/pc/script.coffee
@@ -1,6 +1,8 @@
 class Main
     constructor: ->
         @$firstview = []
+        @$contents = $(".contents")
+        @$result = $(".result")
 
         for i in [0...$(".firstview").size()]
             @$firstview.push $(".firstview").filter("[data-id=\"#{i + 1}\"]")
@@ -17,8 +19,20 @@ class Main
             @$firstview[0].velocity opacity: 0, DUR, => @$firstview[0].hide()
             @$firstview[1].show().velocity opacity: 1, DUR
 
-        @$firstview[1].find(".firstview_start").on "click", ->
-            alert "現在製作中"
+        @$firstview[1].find(".firstview_start").on "click", =>
+            @$firstview[1].velocity opacity: 0, DUR, =>
+                @$firstview[1].hide()
+                @$contents.addClass("show_result").velocity(
+                    backgroundColor: "#FC363B", DUR
+                )
+                @$result.attr "data-id": "1"
+
+                _num = parseInt(@$firstview[1].find(".firstview_input_inner").val()) / 500
+                _tmpl = $(".result_item_tmpl").html()
+                _inner_html = ""
+                for i in [0..._num]
+                    _inner_html += _tmpl
+                @$result.find(".result_item_container").html _inner_html
 
         ###########################
         #   INIT
--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ Main = (function() {
   function Main() {
     var i, j, ref;
     this.$firstview = [];
+    this.$contents = $(".contents");
+    this.$result = $(".result");
     for (i = j = 0, ref = $(".firstview").size(); 0 <= ref ? j < ref : j > ref; i = 0 <= ref ? ++j : --j) {
       this.$firstview.push($(".firstview").filter("[data-id=\"" + (i + 1) + "\"]"));
     }
@@ -24,9 +26,29 @@ Main = (function() {
         }, DUR);
       };
     })(this));
-    return this.$firstview[1].find(".firstview_start").on("click", function() {
-      return alert("現在製作中");
-    });
+    return this.$firstview[1].find(".firstview_start").on("click", (function(_this) {
+      return function() {
+        return _this.$firstview[1].velocity({
+          opacity: 0
+        }, DUR, function() {
+          var _inner_html, _num, _tmpl, i, j, ref;
+          _this.$firstview[1].hide();
+          _this.$contents.addClass("show_result").velocity({
+            backgroundColor: "#FC363B"
+          }, DUR);
+          _this.$result.attr({
+            "data-id": "1"
+          });
+          _num = parseInt(_this.$firstview[1].find(".firstview_input_inner").val()) / 500;
+          _tmpl = $(".result_item_tmpl").html();
+          _inner_html = "";
+          for (i = j = 0, ref = _num; 0 <= ref ? j < ref : j > ref; i = 0 <= ref ? ++j : --j) {
+            _inner_html += _tmpl;
+          }
+          return _this.$result.find(".result_item_container").html(_inner_html);
+        });
+      };
+    })(this));
   };
 
   return Main;
