bdcec48aaa54fa3391be34ef046dfc5cceb8f9f8
Tajima Shingo <tajima-shingo@kayac.com>
2015-11-19 09:47:52 +0900

    結果画像を背景のリピートに

--- _build/dist/js/script.js
+++ _build/dist/js/script.js
@@ -4,8 +4,15 @@ Main = (function() {
   function Main() {
     var i, j, ref;
     this.$firstview = [];
+    this.$win = $(window);
+    this.$header = $(".header");
     this.$contents = $(".contents");
     this.$result = $(".result");
+    this.$result_price_num = $(".result_price_num");
+    this.$result_item = $(".result_item");
+    this.$result_formula_price = $(".result_formula_price");
+    this.$result_formula_amount = $(".result_formula_amount");
+    this.$result_formula_unit = $(".result_formula_unit");
     for (i = j = 0, ref = $(".firstview").size(); 0 <= ref ? j < ref : j > ref; i = 0 <= ref ? ++j : --j) {
       this.$firstview.push($(".firstview").filter("[data-id=\"" + (i + 1) + "\"]"));
     }
@@ -19,34 +26,55 @@ Main = (function() {
         _this.$firstview[0].velocity({
           opacity: 0
         }, DUR, function() {
-          return _this.$firstview[0].hide();
+          _this.$firstview[0].hide();
+          return _this.$firstview[1].find(".firstview_input_inner").focus();
         });
         return _this.$firstview[1].show().velocity({
           opacity: 1
         }, DUR);
       };
     })(this));
-    return this.$firstview[1].find(".firstview_start").on("click", (function(_this) {
+    this.$firstview[1].find(".firstview_start").on("click", (function(_this) {
       return function() {
+        if (isNaN(parseInt(_this.$firstview[1].find(".firstview_input_inner").val()))) {
+          alert("数値を適切に入力してください。");
+          return;
+        }
+        _this.$firstview[1].find(".firstview_start").off("click");
         return _this.$firstview[1].velocity({
           opacity: 0
         }, DUR, function() {
-          var _inner_html, _num, _tmpl, i, j, ref;
+          var _item_price, _price, _separated_price;
           _this.$firstview[1].hide();
+          _this.$header.addClass("show_result");
           _this.$contents.addClass("show_result").velocity({
             backgroundColor: "#FC363B"
           }, DUR);
-          _this.$result.attr({
-            "data-id": "1"
+          _this.$result.show().attr({
+            "data-id": "beer"
           });
-          _num = parseInt(_this.$firstview[1].find(".firstview_input_inner").val()) / 500;
-          _tmpl = $(".result_item_tmpl").html();
-          _inner_html = "";
-          for (i = j = 0, ref = _num; 0 <= ref ? j < ref : j > ref; i = 0 <= ref ? ++j : --j) {
-            _inner_html += _tmpl;
-          }
-          return _this.$result.find(".result_item_container").html(_inner_html);
+          _price = parseInt(_this.$firstview[1].find(".firstview_input_inner").val());
+          _separated_price = String(_price).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+          _item_price = 500;
+          _this.$result_price_num.text(_separated_price);
+          _this.$result_formula_price.text(_separated_price);
+          _this.$result_formula_amount.attr({
+            "data-id": "beer"
+          }).text(String(Math.floor(_price / _item_price)).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
+          _this.$result_formula_unit.text("杯分");
+          return _this.$result_item.height(Math.ceil(Math.floor(_price / _item_price) / 10) * (_this.$result_item.width() / 10));
         });
+      };
+    })(this));
+    return this.$win.on("keydown", (function(_this) {
+      return function(e) {
+        if (e.keyCode === 13) {
+          if (_this.$firstview[0].css("display") === "block") {
+            return _this.$firstview[0].find(".firstview_start").trigger("click");
+          } else if (_this.$firstview[1].css("display") === "block") {
+            return _this.$firstview[1].find(".firstview_start").trigger("click");
+          }
+        }
       };
     })(this));
   };
--- _build/src/coffee/pc/script.coffee
+++ _build/src/coffee/pc/script.coffee
@@ -1,8 +1,15 @@
 class Main
     constructor: ->
         @$firstview = []
+        @$win = $(window)
+        @$header = $(".header")
         @$contents = $(".contents")
         @$result = $(".result")
+        @$result_price_num = $(".result_price_num")
+        @$result_item = $(".result_item")
+        @$result_formula_price = $(".result_formula_price")
+        @$result_formula_amount = $(".result_formula_amount")
+        @$result_formula_unit = $(".result_formula_unit")
 
         for i in [0...$(".firstview").size()]
             @$firstview.push $(".firstview").filter("[data-id=\"#{i + 1}\"]")
@@ -16,23 +23,49 @@ class Main
         ###########################
 
         @$firstview[0].find(".firstview_start").one "click", =>
-            @$firstview[0].velocity opacity: 0, DUR, => @$firstview[0].hide()
+            @$firstview[0].velocity opacity: 0, DUR, =>
+                @$firstview[0].hide()
+                @$firstview[1].find(".firstview_input_inner").focus()
             @$firstview[1].show().velocity opacity: 1, DUR
 
         @$firstview[1].find(".firstview_start").on "click", =>
+            if isNaN parseInt(@$firstview[1].find(".firstview_input_inner").val())
+                alert "数値を適切に入力してください。"
+                return
+            @$firstview[1].find(".firstview_start").off "click"
+
             @$firstview[1].velocity opacity: 0, DUR, =>
                 @$firstview[1].hide()
-                @$contents.addClass("show_result").velocity(
-                    backgroundColor: "#FC363B", DUR
-                )
-                @$result.attr "data-id": "1"
+                @$header.addClass "show_result"
+                @$contents.addClass("show_result").velocity backgroundColor: "#FC363B", DUR
 
-                _num = parseInt(@$firstview[1].find(".firstview_input_inner").val()) / 500
-                _tmpl = $(".result_item_tmpl").html()
-                _inner_html = ""
-                for i in [0..._num]
-                    _inner_html += _tmpl
-                @$result.find(".result_item_container").html _inner_html
+                # 結果表示
+                @$result.show().attr "data-id": "beer"
+
+                _price = parseInt(@$firstview[1].find(".firstview_input_inner").val())
+
+                # 3桁カンマ区切り
+                _separated_price = String(_price).replace /(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"
+                _item_price = 500
+
+                @$result_price_num.text _separated_price
+                @$result_formula_price.text _separated_price
+                @$result_formula_amount.attr("data-id": "beer").text(
+                    String(Math.floor(_price / _item_price)).replace /(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"
+                )
+                @$result_formula_unit.text "杯分"
+
+                # (@$result_item.width() / 10) はアイテム1個あたりの画像幅
+                @$result_item.height(
+                    Math.ceil(Math.floor(_price / _item_price) / 10) * (@$result_item.width() / 10)
+                )
+
+        @$win.on "keydown", (e) =>
+            if e.keyCode == 13
+                if @$firstview[0].css("display") == "block"
+                    @$firstview[0].find(".firstview_start").trigger "click"
+                else if @$firstview[1].css("display") == "block"
+                    @$firstview[1].find(".firstview_start").trigger "click"
 
         ###########################
         #   INIT
